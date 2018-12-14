a58c0fcc283df3b8b86a5a74ebdbd45c22b7a781
Abhilash <abhilash456a@live.com>
2018-12-14 19:02:05 +0530

    End of section 12

--- DattingApp.Api/Dto/UserForRegisterDto.cs
+++ DattingApp.Api/Dto/UserForRegisterDto.cs
@@ -14,5 +14,27 @@ namespace DatingApp.Api.Dto
         [Required]
         [StringLength(8, MinimumLength =4, ErrorMessage ="You must specify password between 4 to 8 characters")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
--- DattingApp.Api/Helpers/AutoMapperProfiles.cs
+++ DattingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -34,6 +34,7 @@ namespace DatingApp.Api.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
 
     }
