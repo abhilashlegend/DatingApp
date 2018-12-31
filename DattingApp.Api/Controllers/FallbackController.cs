1d33102dcdae390290e422ed6b100a0a81105253
Abhilash <abhilash456a@live.com>
2018-12-31 19:29:10 +0530

    publishing

---
+++ DattingApp.Api/Controllers/FallbackController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Api.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+                "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
